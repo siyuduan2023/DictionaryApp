ca08143f84793b9f5c7587a18fde4a05883c467d
Siyu Duan <siyusiyuduan@hotmail.com>
2024-01-12 16:44:42 +0000

    add comments and css

--- main.js
+++ main.js
@@ -2,13 +2,16 @@
 //   input: nothing
 //   output: word data
 
+// store the input value
 let inpWord = document.getElementById("inp").value;
 
+// store external API URL
 const apiURL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 const searchBox = document.querySelector("#inp");
 const searchBtn = document.querySelector("#search-btn");
 
+// create function to fetch data
 async function fetchWordData() {
   const response = await fetch(apiURL + searchBox.value);
 
@@ -20,24 +23,23 @@ async function fetchWordData() {
 }
 console.log(fetchWordData());
 
-// function: display feched info on the page
+// function: display fetched info on the page
 //   input: fetched info
 //   output: nothing (does sth, doesn't produce a value)
 function displayWordData(randomInput) {
-    const word = randomInput[0].word;
-    const pos = randomInput[0].meanings[0].partOfSpeech;
-    const sound = randomInput[0].phonetic;
-    const meaning = randomInput[0].meanings[0].definitions[0].definition;
-  
-   
-    const wordsElement = document.querySelector("#words");
-    const posElement = document.querySelector("#pos");
-    const soundElement = document.querySelector("#sound");
-    const meaningElement = document.querySelector("#meaning");
-    wordsElement.textContent = `${word}`;
-    posElement.textContent = `${pos}`;
-    soundElement.textContent = `${sound}`;
-    meaningElement.textContent = `Meaning: ${meaning}`;
+  const word = randomInput[0].word.toLowerCase();
+  const pos = randomInput[0].meanings[0].partOfSpeech;
+  const sound = randomInput[0].phonetic;
+  const meaning = randomInput[0].meanings[0].definitions[0].definition;
+
+  const wordsElement = document.querySelector("#words");
+  const posElement = document.querySelector("#pos");
+  const soundElement = document.querySelector("#sound");
+  const meaningElement = document.querySelector("#meaning");
+  wordsElement.textContent = `${word}`;
+  posElement.textContent = `${pos}`;
+  soundElement.textContent = `${sound}`;
+  meaningElement.textContent = `Meaning: ${meaning}`;
 }
 
 //step 2: extract data
@@ -48,10 +50,7 @@ async function fetchAndDisplayWordData() {
 
 fetchAndDisplayWordData();
 
-//get a reference to the button element
-
-searchBtn.addEventListener("click", ()=>{
-    fetchAndDisplayWordData(searchBox.value);
+//add event listener to the button
+searchBtn.addEventListener("click", () => {
+  fetchAndDisplayWordData(searchBox.value);
 });
-
-
